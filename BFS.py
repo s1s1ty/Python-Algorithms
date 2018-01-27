9b4953515f6492a4bcbaa3aff264303e84c8609d
Shaonty <shaonty@Shaontys-MacBook-Pro.local>
2018-01-27 23:44:25 +0600

    Bfs file add

---
+++ BFS.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+class BFS(object):
+	"""Basic Matrix implementation of BFS"""
+	SIZE = 10
+	def __init__(self):
+		self.visited = [0]*BFS.SIZE
+		self.cost = [0]*BFS.SIZE
+		self.am = []
+		for i in range(BFS.SIZE):
+			self.temp = []
+			for j in range(BFS.SIZE):
+				self.temp.append(0)
+			self.am.append(self.temp)
+
+	def bfs(self, souce):
+		self.que = deque([])
+		self.visited[souce] = 1
+		self.que.append(souce)
+
+		while self.que:
+			u = self.que.popleft()
+			for v in range(BFS.SIZE):
+				if self.am[u][v] == 1 and self.visited[v]==0:
+					self.visited[v] = 1
+					self.que.append(v)
+					self.cost[v] = self.cost[u]+1 
+
+	def cost_print(self, destination):
+		return self.cost[destination]
+
+
+ob = BFS()
+
+souce = int(raw_input())
+destination = int(raw_input())
+
+n = int(raw_input())
+
+for i in range(n):
+	tmp = raw_input()
+	u, v = tmp.split()
+	u = int(u)
+	v = int(v)
+	am[u][v] = am[v][u] = 1
+
+# ob.am[1][2] = 1
+# ob.am[1][3] = 1
+# ob.am[1][5] = 1
+# ob.am[3][5] = 1
+# ob.am[8][4] = 1
+# ob.am[2][8] = 1
+# ob.am[3][4] = 1
+
+ob.bfs(souce)
+print ob.cost_print(destination)
