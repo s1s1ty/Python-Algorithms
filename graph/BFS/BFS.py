51a3614cdb71e9cf8e3f67c33a343aef379cd7e6
s1s1ty <shaonty.dutta@gmail.com>
2019-07-08 00:41:55 +0600

    Updated BFS

--- graph/BFS/BFS.py
+++ graph/BFS/BFS.py
@@ -1,29 +1,56 @@
-from collections import deque
+'''
+Shaonty Dutta
+Python BFS Implementation
+Time complexity O(v+e)
+'''
+
+from collections import deque, defaultdict
+
 
 class BFS(object):
+	"""List implementation of BFS"""
+	def __init__(self):
+		self.cost = defaultdict(int)
+	
+	def bfs(self, graph, source):
+		visited, queue = set(), deque([source])
+		visited.add(source)
+		self.cost[source] = 0
+
+		while queue:
+			node = queue.popleft()
+			for neighbour in graph[node]:
+				if neighbour not in visited:
+					visited.add(neighbour)
+					queue.append(neighbour)
+					self.cost[neighbour] = self.cost[node] + 1
+	
+	def cost_print(self, destination):
+		return self.cost[destination]
+	
+
+class BFSM(object):
 	"""Basic Matrix implementation of BFS"""
 	SIZE = 10
 	def __init__(self):
-		self.visited = [0]*BFS.SIZE
-		self.cost = [0]*BFS.SIZE
+		self.cost = [0]*BFSM.SIZE
 		self.am = []
-		for i in range(BFS.SIZE):
+		for i in range(BFSM.SIZE):
 			self.temp = []
-			for j in range(BFS.SIZE):
+			for j in range(BFSM.SIZE):
 				self.temp.append(0)
 			self.am.append(self.temp)
 
 	def bfs(self, source):
-		self.que = deque([])
-		self.visited[source] = 1
-		self.que.append(source)
+		visited, queue = set(), deque([source])
+		visited.add(source)
 
-		while self.que:
-			u = self.que.popleft()
-			for v in range(BFS.SIZE):
-				if self.am[u][v] == 1 and self.visited[v]==0:
-					self.visited[v] = 1
-					self.que.append(v)
+		while queue:
+			u = queue.popleft()
+			for v in range(BFSM.SIZE):
+				if self.am[u][v] == 1 and v not in visited:
+					visited.add(v)
+					queue.append(v)
 					self.cost[v] = self.cost[u]+1 
 
 	def cost_print(self, destination):
--- graph/BFS/test_BFS.py
+++ graph/BFS/test_BFS.py
@@ -1,10 +1,26 @@
 import unittest
-from BFS import BFS
+import collections
+from BFS import BFSM, BFS
 
 class BFSTestCase(unittest.TestCase):
-    """ Test for BFS.py"""
     def test(self):
         ob = BFS()
+        graph = collections.defaultdict(set)
+        graph[1] = [2, 3, 5]
+        graph[2] = [8]
+        graph[3] = [5,4]
+        graph[8] = [4]
+        source = 2
+        destination = 4
+        
+        ob.bfs(graph, source)
+        self.assertEqual(ob.cost_print(destination), 2, msg="Cost should be 2")
+
+
+class BFSMTestCase(unittest.TestCase):
+    """ Test for BFS.py"""
+    def test(self):
+        ob = BFSM()
         # test data 
         ob.am[1][2] = 1
         ob.am[1][3] = 1
